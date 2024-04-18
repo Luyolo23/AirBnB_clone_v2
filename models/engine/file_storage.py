c97067ea0bf31fbe51f5ceaceaaef6c28517401b
Luyolo23 <luyolo.tuta23@gmail.com>
2024-04-18 00:37:35 +0000

    SQLAlchemy please work now...

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""ontains the FileStorage class
+"""contains the FileStorage class
 """
 
 import json
@@ -11,8 +11,8 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-model_classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-                 "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
@@ -21,50 +21,49 @@ class FileStorage:
     # string - path to the JSON file
     __file_path = "file.json"
     # dictionary - empty but will store all objects by <class name>.id
-    __objects_dict = {}
+    __objects = {}
 
-    def all_objects(self, cls=None):
-        """returns the dictionary __objects_dict"""
+    def all(self, cls=None):
+        """returns the dictionary __objects"""
         if cls is not None:
-            filtered_dict = {}
-            for key, value in self.__objects_dict.items():
+            new_dict = {}
+            for key, value in self.__objects.items():
                 if cls == value.__class__ or cls == value.__class__.__name__:
-                    filtered_dict[key] = value
-            return filtered_dict
-        return self.__objects_dict
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
 
-    def add_new_object(self, obj):
-        """sets in __objects_dict the obj with key <obj class name>.id"""
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
-            self.__objects_dict[key] = obj
+            self.__objects[key] = obj
 
-    def save_objects(self):
-        """serializes __objects_dict to the JSON file (path: __file_path)"""
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
-        for key in self.__objects_dict:
-            json_objects[key] = self.__objects_dict[key].to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(json_objects, file)
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as fl:
+            json.dump(json_objects, fl)
 
-    def reload_objects(self):
-        """deserializes the JSON file to __objects_dict"""
+    def reload(self):
+        """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as file:
-                json_objects = json.load(file)
-            for key in json_objects:
-                self.__objects_dict[key] = model_classes[json_objects[key]["__class__"]](**json_objects[key])
+            with open(self.__file_path, 'r') as fl:
+                j_o = json.load(fl)
+            for key in j_o:
+                self.__objects[key] = classes[j_o[key]["__class__"]](**j_o[key])
         except:
             pass
 
-    def delete_object(self, obj=None):
-        """delete obj from __objects_dict if it’s inside"""
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects_dict:
-                del self.__objects_dict[key]
+            if key in self.__objects:
+                del self.__objects[key]
 
-    def close_storage(self):
-        """call reload_objects() method for deserializing the JSON file to objects"""
-        self.reload_objects()
-
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
